4524d05494b97b6765b074306223a59a189bce53
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2022-06-01 12:04:00 -0300

    first commit

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1654095700,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tilesetpiso.png",
+		"images/spritepersonagem-sheet0.png",
+		"images/spriteobstaculo-sheet0.png",
+		"images/objetopiso.png",
+		"images/spriteperseguidor-sheet0.png",
+		"images/backgroundjogo.png",
+		"images/spriteitem-sheet0.png",
+		"images/spriteitem-sheet1.png",
+		"images/backgroundmorte-sheet0.png",
+		"images/spriteescudo-sheet0.png",
+		"images/spritebanco-sheet0.png",
+		"images/spritefontplacar.png",
+		"images/spritefont.png",
+		"images/backgroundcréditos-sheet0.png",
+		"images/backgroundmenu-sheet0.png",
+		"images/spritetelatutorial-sheet0.png",
+		"media/game.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
